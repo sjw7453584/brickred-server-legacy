36384e5459b3f0cf950e559947808b76ec210756
kaien <kaienkira@gmail.com>
2014-12-05 10:55:27 +0800

    add brickred::os::signal

--- src/brickred/unix/system.cc
+++ src/brickred/unix/system.cc
@@ -4,6 +4,7 @@
 #include <unistd.h>
 #include <sys/stat.h>
 #include <sys/types.h>
+#include <csignal>
 #include <cstdio>
 
 namespace brickred {
@@ -65,6 +66,28 @@ bool createPidFile(const char *file)
     ::fclose(fp);
 
     return true;
+}
+
+SignalHandler signal(int signum, SignalHandler sighandler)
+{
+    struct sigaction sa_new;
+    struct sigaction sa_old;
+
+    sa_new.sa_handler = sighandler;
+    sigemptyset(&sa_new.sa_mask);
+    sa_new.sa_flags = 0;
+#ifdef SA_INTERRUPT
+    sa_new.sa_flags |= SA_INTERRUPT;
+#endif
+
+    if (sigaction(signum, &sa_new, &sa_old) != 0)
+    {
+        return SIG_ERR;
+    }
+    else
+    {
+        return sa_old.sa_handler;
+    }
 }
 
 } // namespace os
--- src/brickred/unix/system.h
+++ src/brickred/unix/system.h
@@ -7,6 +7,9 @@ namespace os {
 bool daemon(bool change_dir = false, bool close_stdio = false);
 bool createPidFile(const char *file);
 
+typedef void (*SignalHandler)(int);
+SignalHandler signal(int signum, SignalHandler sighandler);
+
 } // namespace os
 } // namespace brickred
 
